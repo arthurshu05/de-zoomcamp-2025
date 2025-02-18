ef75757c5532893bc4c0c08e316fd8f309c4ea6a
Arthur <arthurshu05@gmail.com>
2025-02-18 16:36:06 -0500

    Updated to module 1.2.4
    - Use a script to ingest taxi data to DB
    - Create docker image to use the script and ingest data to DB
    
    !! Causion!!
    The new file is using parquet so the docker file is a bit different from
    the course.

---
+++ docker-sql/ingest_data.py
@@ -0,0 +1,65 @@
+import os
+import argparse
+
+from time import time
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+
+    parquet_file = 'output-parquet.parquet'
+    csv_file = 'output.csv'
+
+    os.system(f'wget {url} -O {parquet_file}')
+
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    df = pd.read_parquet(parquet_file)
+    df.to_csv(csv_file)
+
+    df_iter = pd.read_csv(csv_file, iterator=True, chunksize=100000)
+
+    df = next(df_iter)
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+    while True:
+        t_start = time()
+        
+        df = next(df_iter)
+
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+        df.to_sql(name=table_name, con=engine, if_exists='append')
+
+        t_end = time()
+
+        print('inserted another chunk..., took %.3f second' % (t_end - t_start))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+
+    parser.add_argument('--user', help='user name for postgres')
+    parser.add_argument('--password', help='password name for postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port for postgres')
+    parser.add_argument('--db', help='database name for postgres')
+    parser.add_argument('--table_name', help='name of the table where we will write the results to')
+    parser.add_argument('--url', help='url of the csv file')
+
+    args = parser.parse_args()
+    main(args)
+
